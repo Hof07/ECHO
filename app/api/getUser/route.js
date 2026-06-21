5fdc53f7fe4cae02dbf48ecff9e2d0b3dab43d19
Hof07 <dixitansh436@gmail.com>
2026-06-22 00:37:45 +0530

    temp

--- app/api/getUser/route.js
+++ app/api/getUser/route.js
@@ -11,7 +11,7 @@ const supabase = createClient(
 
 // Use server-only secret (remove NEXT_PUBLIC_ prefix in production)
 const SECRET_ENV_NAME = "JWT_SECRET"; // recommend this
-const rawSecret = process.env.JWT_SECRET;
+const rawSecret = "hjknkjsndkjnkjIdjnfkjnfkjLOVE_kfosjyoudnskjndsjnUtsavidjwndkjekdnkejkjehrkjerjnj";
 
 if (!rawSecret) {
   console.error(`[getUser] Missing env var ${SECRET_ENV_NAME}`);
